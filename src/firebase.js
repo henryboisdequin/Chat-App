506249ea84ee3b8c33a6a1b676e004073c3084dc
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-09-20 12:12:13 +0530

    clean up

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAhW6uUkif563kL8QwfonYVWH522j0dsG4",
+  authDomain: "chat-app-91f7e.firebaseapp.com",
+  databaseURL: "https://chat-app-91f7e.firebaseio.com",
+  projectId: "chat-app-91f7e",
+  storageBucket: "chat-app-91f7e.appspot.com",
+  messagingSenderId: "189960763216",
+  appId: "1:189960763216:web:e6b597b792e79a0ea75e55",
+  measurementId: "G-2W41LVK5DR",
+};
