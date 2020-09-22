e8edc14e79210df562e7c58f96573b7980623558
Henry Boisdequin <65845077+henryboisdequin@users.noreply.github.com>
2020-09-22 07:57:08 +0530

    finished chat-header

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import { SearchOutlined, AttachFile, MoreVert } from "@material-ui/icons";
 
 export default function Chat() {
+  const [seed, setSeed] = useState("");
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 1000));
+  }, []);
+
   return (
     <div className="chat">
-      <h1>Chat</h1>
+      <div className="chat-header">
+        <Avatar src={`https://avatars.dicebear.com/api/bottts/${seed}.svg`} />
+        <div className="chat-headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at ....</p>
+        </div>
+        <div className="chat-headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat-body"></div>
+      <div className="chat-footer"></div>
     </div>
   );
 }
